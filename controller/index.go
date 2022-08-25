4b338abbcea7b7b2166b59f7aa2987cadd364d92
kavanahuang <kernelhuang@outlook.com>
2022-08-25 22:47:43 +0800

    Initial project.

---
+++ controller/index.go
@@ -0,0 +1,34 @@
+/**
+ * Created by Kernel.Huang.
+ * User: kernel@live.com
+ * Date: 2019/8/26
+ * Time: 14:13
+ */
+
+package controller
+
+import (
+	"github.com/kataras/iris/v12"
+	"github.com/kavanahuang/status"
+)
+
+type IndexController struct {
+	data interface{}
+}
+
+func (index *IndexController) IndexAction(ctx iris.Context) error {
+	body, err := ctx.GetBody()
+	if err != nil {
+		return err
+	}
+
+	status.OK.Data = string(body)
+	_, err = ctx.JSON(status.OK)
+	return err
+}
+
+func (index *IndexController) HomeAction(ctx iris.Context) error {
+	status.OK.Data = ctx.Params().Get("name")
+	_, err := ctx.JSON(status.OK)
+	return err
+}
